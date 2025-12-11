59d6e3586a65be7475c43aa8ec2eb541de5c8dc4
Fooxii <ironata07@gmail.com>
2025-12-11 14:28:17 -0400

    2nd attempt to fix script in 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,10 +9,11 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 size_t left_h, right_h;
-if (tree == (void *)0)
+if (!tree || (!tree->left && !tree->right))
 {
 return (0);
 }
+
 left_h = binary_tree_height(tree->left);
 right_h = binary_tree_height(tree->right);
 
