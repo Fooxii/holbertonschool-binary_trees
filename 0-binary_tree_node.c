6dc6f3e592aa0d25b9731c6b6d04662182d80819
Fooxii <ironata07@gmail.com>
2025-12-10 20:19:05 -0400

    Fixed script in 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,10 +12,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 binary_tree_t *newnode;
 int cpvalue;
-if (!parent)
-{
-return ((void *)0);
-}
 
 newnode = malloc(sizeof(binary_tree_t));
 if (!newnode)
