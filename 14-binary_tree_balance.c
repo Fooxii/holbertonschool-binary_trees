3d46e8a5c7a8ac35ba1b8b1cecaf49d36f271c9e
Fooxii <ironata07@gmail.com>
2025-12-12 16:30:42 -0400

    8th attempt to fix script in 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - measures the height of a binary tree
+ * binary_tree_height_v2 - measures the height of a binary tree
  * @tree: pointer to the root node of the tree
  * Return: height of the tree, 0 if tree is NULL or a leaf
  */
@@ -10,21 +10,15 @@ int binary_tree_height_v2(const binary_tree_t *tree)
 {
 int left_h, right_h;
 
-if (!tree || (!tree->left && !tree->right))
+if (!tree)
 return (0);
 
 left_h = binary_tree_height_v2(tree->left);
 right_h = binary_tree_height_v2(tree->right);
 
-if (left_h > right_h)
-{
-return (left_h + 1);
+return ( (left_h > right_h ? left_h : right_h) + 1 );
 }
-else
-{
-return (right_h + 1);
-}
-}
+
 
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
