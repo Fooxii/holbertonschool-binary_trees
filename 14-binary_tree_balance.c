cf07f649bdba1de54298dc54edb644404e674116
Fooxii <ironata07@gmail.com>
2025-12-12 15:40:50 -0400

    5th attempt to fix script in 14-binary_tree_balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,39 +2,30 @@
 
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node
- * Return: balance factor (left height - right height)
+ * @tree: pointer to the root node of the tree
+ * Return: balance factor (height of left subtree - height of right subtree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-int left_h = 0, right_h = 0;
+size_t left_h = 0, right_h = 0;
+size_t lh_left, lh_right, rh_left, rh_right;
 
 if (!tree)
 return (0);
 
 if (tree->left)
 {
-int lh_left = 0, lh_right = 0;
-
-if (tree->left->left)
-lh_left = 1 + binary_tree_balance(tree->left->left);
-if (tree->left->right)
-lh_right = 1 + binary_tree_balance(tree->left->right);
-
-left_h = (lh_left > lh_right ? lh_left : lh_right) + 1;
+lh_left = tree->left->left ? 1 + binary_tree_balance(tree->left->left) : 1;
+lh_right = tree->left->right ? 1 + binary_tree_balance(tree->left->right) : 1;
+left_h = (lh_left > lh_right ? lh_left : lh_right);
 }
 
 if (tree->right)
 {
-int rh_left = 0, rh_right = 0;
-
-if (tree->right->left)
-rh_left = 1 + binary_tree_balance(tree->right->left);
-if (tree->right->right)
-rh_right = 1 + binary_tree_balance(tree->right->right);
-
-right_h = (rh_left > rh_right ? rh_left : rh_right) + 1;
+rh_left = tree->right->left ? 1 + binary_tree_balance(tree->right->left) : 1;
+rh_right = tree->right->right ? 1 + binary_tree_balance(tree->right->right) : 1;
+right_h = (rh_left > rh_right ? rh_left : rh_right);
 }
 
-return left_h - right_h;
+return ((int)left_h - (int)right_h);
 }
