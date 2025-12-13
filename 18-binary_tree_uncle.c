92d7b2eba3ca36fd7d8abe201bcd83a4fdd54b99
Fooxii <ironata07@gmail.com>
2025-12-12 23:12:15 -0400

    Fixed script in 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,6 +1,30 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_sibling2 - function that finds the sibling of a node
+ * @node: pointer to the node being used to find its sibling
+ * Return: pointer to the sibling node, NULL if no sibling
+ */
+
+binary_tree_t *binary_tree_sibling2(binary_tree_t *node)
+{
+if (!node || !node->parent)
+{
+return ((void *)0);
+}
+
+if (node->parent->left && node->parent->left != node)
+{
+return (node->parent->left);
+}
+if (node->parent->right && node->parent->right != node)
+{
+return (node->parent->right);
+}
+return ((void *)0);
+}
+
+/**
  * binary_tree_uncle - function that finds the uncle of a node
  * @node: pointer to the node being used to find its uncle
  * Return: pointer to the unclde node, NULL if no uncle
@@ -13,5 +37,5 @@ if (!node || !node->parent || !node->parent->parent)
 return ((void *)0);
 }
 
-return (binary_tree_sibling(node->parent));
+return (binary_tree_sibling2(node->parent));
 }
