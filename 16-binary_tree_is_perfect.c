5b05a12172c471be12dc387704848827d812c313
Fooxii <ironata07@gmail.com>
2025-12-12 22:15:43 -0400

    Check if leafs are in same level by getting the max height and comparing each leaf to that height

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,41 +1,32 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height_v2 - measures the height of a binary tree
- * @tree: pointer to the root node of the tree
- * Return: height of the tree, 0 if tree is NULL or a leaf
+ * binary_tree_height - function that measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of binary tree
  */
 
-int binary_tree_height_v2(const binary_tree_t *tree)
+int binary_tree_height2(const binary_tree_t *tree)
 {
 int left_h, right_h;
-
-if (!tree)
-return (0);
-
-left_h = binary_tree_height_v2(tree->left);
-right_h = binary_tree_height_v2(tree->right);
-
-return ((left_h > right_h ? left_h : right_h) +1);
-}
-
-/**
- * binary_tree_balance2 - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree
- * Return: balance factor (left height - right height), 0 if tree is NULL
- */
-
-int binary_tree_balance2(const binary_tree_t *tree)
+if (!tree || (!tree->left && !tree->right))
 {
-int left_h, right_h;
-if (!tree)
 return (0);
-
-left_h = binary_tree_height_v2(tree->left);
-right_h = binary_tree_height_v2(tree->right);
-
-return (left_h - right_h);
 }
+
+left_h = binary_tree_height2(tree->left);
+right_h = binary_tree_height2(tree->right);
+
+if (left_h > right_h)
+{
+return (left_h + 1);
+}
+else
+{
+return (right_h + 1);
+}
+}
+
 
 /**
  * binary_tree_is_full2 - function that checks if a binary tree is full
@@ -69,6 +60,7 @@ return (0);
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+int leaf_h = binary_tree_height2(tree);
 if (!tree)
 {
 return (0);
@@ -77,10 +69,15 @@ if (binary_tree_is_full2(tree) != 1)
 {
 return (0);
 }
-if (binary_tree_balance2(tree) != 0)
+
+if (!tree->left && !tree->right)
+{
+if (binary_tree_height2(tree) != leaf_h)
 {
 return (0);
 }
-return (1);
+}
 
+
+return (1);
 }
